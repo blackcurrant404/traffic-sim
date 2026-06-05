67a74e5654ee1c23cf7f7e98a25608ac82d56017
blackcurrant404 <255106216+blackcurrant404@users.noreply.github.com>
2026-06-05 16:59:48 +0300

    add parser for converting CSV to structured dictionary

---
+++ data-loader.py
@@ -0,0 +1,14 @@
+import csv
+
+def csv_to_dictionary():
+    users = {}
+    with open("data/users.csv") as new_file:
+        for line in csv.reader(new_file, delimiter=","):
+            if line[0] == "Username" or line[0] == "":
+                continue
+            users[line[0]] = {
+                "correct_password": line[1],
+                "wrong_passwords": line[2:]
+            }
+                
+    return users
